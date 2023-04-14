d72ee4cb62d5c139d9b4f88916ed0a2153cd8e81
Xnork <xnordeuzivan@gmail.com>
2023-04-14 01:07:43 -0300

    (update): Implement login screen base

--- frontend/pages/login.tsx
+++ frontend/pages/login.tsx
@@ -7,6 +7,13 @@ const inter = Inter({ subsets: ['latin'] })
 export default function Login() {
   return (
     <Layout>
+        <div className='login-card'>
+          <label>E-MAIL</label>
+          <input type='email' placeholder='exemplo@gmail.com'/>
+          <label>SENHA</label>
+          <input type='password' placeholder='********'/>
+          <button>Entrar No Sistema</button>
+        </div>
     </Layout>
   )
 }
