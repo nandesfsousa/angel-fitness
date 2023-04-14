bb1dab1eec9bb2acb9efe51b1a3280b1ee03df4a
Xnork <xnordeuzivan@gmail.com>
2023-04-14 01:26:40 -0300

    (update): Recreate project

--- frontend/pages/login.tsx
+++ frontend/pages/login.tsx
@@ -1,13 +1,14 @@
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
-import { Layout } from 'components/Layout'
+import { Layout } from '../components/Layout'
+import styles from '../styles/Login.module.css'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Login() {
   return (
-    <Layout>
-        <div className='login-card'>
+    <Layout style={{backgroundColor: '#ef4d64', height: '100vh'}}>
+        <div className={styles.loginCard}>
           <label>E-MAIL</label>
           <input type='email' placeholder='exemplo@gmail.com'/>
           <label>SENHA</label>
