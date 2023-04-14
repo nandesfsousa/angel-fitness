bd977bb111f92ec31d5065eadcf4d2ab74329165
Xnork <xnordeuzivan@gmail.com>
2023-04-14 14:10:11 -0300

    (update): add navigation button style

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -4,31 +4,34 @@ import { Inter } from 'next/font/google'
 import { Layout } from '@/components/Layout'
 import { ActionBar } from '@/components/ActionBar'
 import styles from '@/styles/Home.module.css'
+import { useEffect, useState } from 'react'
 
 const inter = Inter({ subsets: ['latin'] })
 
-const students = [
-  {
-    name: 'Deuzivan Lima Viana',
-    email: 'deuzivan@gmail.com',
-    age: 23,
-    is_active: false
-  },
-  {
-    name: 'Nandes Sousa Fernandes de Sousa',
-    email: 'nandes@gmail.com',
-    age: 27,
-    is_active: true
-  },
-  {
-    name: 'Friedrich Wilhelm Nietzsche',
-    email: 'nietzsche@gmail.com',
-    age: 179,
-    is_active: false
-  }
-]
-
 export default function Home() {
+  const [students, setStudents] = useState(
+    [
+      {
+        name: 'Deuzivan Lima Viana',
+        email: 'deuzivan@gmail.com',
+        age: 23,
+        is_active: false
+      },
+      {
+        name: 'Nandes Sousa Fernandes de Sousa',
+        email: 'nandes@gmail.com',
+        age: 27,
+        is_active: true
+      },
+      {
+        name: 'Friedrich Wilhelm Nietzsche',
+        email: 'nietzsche@gmail.com',
+        age: 179,
+        is_active: false
+      }
+    ]
+  )
+
   return <Layout style={{backgroundColor: 'rgb(240, 240, 240)', height: '100vh'}}>
     <ActionBar/>
     <div className={styles.manageStudentContainer}>
@@ -43,33 +46,32 @@ export default function Home() {
       </nav>
 
       <table>
-        <tr>
-          <th>NOME</th>
-          <th>EMAIL</th>
-          <th>IDADE</th>
-          <th>MARTRICULA ATIVA</th>
-          <th></th>
-        </tr>
-        {
-          students.map((student) => {
-            return (<tr>
+        <thead>
+          <tr>
+            <th>NOME</th>
+            <th>EMAIL</th>
+            <th>IDADE</th>
+            <th>MARTRICULA ATIVA</th>
+            <th></th>
+          </tr>
+        </thead>
+        <tbody>
+          {students.map((student) => ( <tr key={student.email}>
               <td>{ student.name }</td>
               <td>{ student.email }</td>
               <td>{ student.age }</td>
               <td>
-                {
-                  student.is_active ?
+                {student.is_active ?
                     <i className={'bi bi-check-circle-fill'} style={{color: '#00ff6a'}}></i> :
-                    <i className={'bi bi-x-circle-fill'} style={{color: '#ff0a2f'}}></i>
-                }
+                    <i className={'bi bi-x-circle-fill'} style={{color: '#ff0a2f'}}></i>}
               </td>
               <td>
                 <button>EDITAR</button>
                 <button>APAGAR</button>
               </td>
             </tr>)
-          })
-        }
+          )}
+        </tbody>
       </table>
       <footer className={styles.naviagtionStudent}>
         <button>ANTERIOR</button>
